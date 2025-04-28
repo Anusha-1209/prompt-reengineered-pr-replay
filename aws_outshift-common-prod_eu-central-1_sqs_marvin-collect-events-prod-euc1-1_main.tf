ab46a7a1ca9c1b363a8bce819c2303dc7db0bacb
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-28 19:42:50 -0400

    -

---
+++ aws_outshift-common-prod_eu-central-1_sqs_marvin-collect-events-prod-euc1-1_main.tf
@@ -0,0 +1,51 @@
+# This provider allows access to the eticloud/eticcprod namespace in Keeper. Do not modify it without discussing with the SRE team.
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+# Change `path = "secret/eticcprod/infra/<account_name>/aws" to specify the account in which the resources will be created.
+# Must match the account in which the VPC was created.
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/outshift-common-prod/terraform_admin"
+}
+
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-prod" # UPDATE ME.
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/outshift-common-prod/us-east-2/s3/marvin-collect-events-prod-euc1-1.tfstate"
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+
+  }
+}
+
+variable "AWS_INFRA_REGION" {
+  description = "AWS Region"
+  default     = "eu-central-1" #Set the region for the resources to be created.
+}
+# Infra AWS Provider
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = var.AWS_INFRA_REGION
+  max_retries = 3
+}
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  name = "marvin-collect-events-prod-euc1-1"
+  fifo_queue = false
+  tags = {
+    CSBDataClassification = "Cisco Restricted"
+    CSBEnvironment        = "Prod"
+    CSBApplicationName    = "Marvin"
+    CSBResourceOwner      = "Outshift SRE"
+    CSBCiscoMailAlias     = "eti-sre@cisco.com"
+    CSBDataTaxonomy       = "Cisco Operations Data"
+  }
+}
