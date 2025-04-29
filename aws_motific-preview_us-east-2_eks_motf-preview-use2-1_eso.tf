1ec4650131182eff165ca3cea80cf005cf70d51b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-29 15:13:09 -0400

    fix: terraform formatting

--- aws_motific-preview_us-east-2_eks_motf-preview-use2-1_eso.tf
+++ aws_motific-preview_us-east-2_eks_motf-preview-use2-1_eso.tf
@@ -5,8 +5,8 @@ provider "vault" {
 }
 
 data "vault_generic_secret" "aws_infra_credential" {
-  provider    = vault.eticloud
-  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
 }
 
 provider "aws" {
@@ -17,31 +17,31 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  depends_on = [ module.eks_all_in_one ]
-  provider  = aws.target
-  name      = local.name
+  depends_on = [module.eks_all_in_one]
+  provider   = aws.target
+  name       = local.name
 }
 
 data "vault_generic_secret" "cluster_certificate" {
-  depends_on = [ module.eks_all_in_one ]
-  provider = vault.eticloud
-  path = "secret/infra/eks/${local.name}/certificate"
+  depends_on = [module.eks_all_in_one]
+  provider   = vault.eticloud
+  path       = "secret/infra/eks/${local.name}/certificate"
 }
 
 module "eso_eticloud" {
-  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
-  cluster_name         = local.name
-  vault_namespace      = "eticloud"
-  kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
-  kubernetes_ca        = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  policies             = ["external-secrets-preview"]
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
+  cluster_name    = local.name
+  vault_namespace = "eticloud"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-preview"]
 }
 
 module "eso_eticloud_apps_vowel" {
-  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
-  cluster_name         = local.name
-  vault_namespace      = "eticloud/apps/vowel"
-  kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
-  kubernetes_ca        = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  policies             = ["external-secrets-preview"]
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
+  cluster_name    = local.name
+  vault_namespace = "eticloud/apps/vowel"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-preview"]
 }
--- aws_motific-preview_us-east-2_eks_motf-preview-use2-1_main.tf
+++ aws_motific-preview_us-east-2_eks_motf-preview-use2-1_main.tf
@@ -1,35 +1,35 @@
 terraform {
   backend "s3" {
     # This is the name of the backend S3 bucket.
-    bucket          = "eticloud-tf-state-nonprod" # UPDATE ME.
+    bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key             = "terraform-state/aws/motific-preview/us-east-2/eks/motf-preview-use2-1.tfstate" # UPDATE ME.
+    key = "terraform-state/aws/motific-preview/us-east-2/eks/motf-preview-use2-1.tfstate" # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
-    region          = "us-east-2" # DO NOT CHANGE.
+    region = "us-east-2" # DO NOT CHANGE.
 
   }
 }
 
 locals {
-  name              = "motf-preview-use2-1"
-  region            = "us-east-2"
-  aws_account_name  = "motific-preview"
+  name             = "motf-preview-use2-1"
+  region           = "us-east-2"
+  aws_account_name = "motific-preview"
 }
 
 module "eks_all_in_one" {
-  source            = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
-  name              = local.name              # EKS cluster name
-  region            = local.region            # AWS provider region
-  aws_account_name  = local.aws_account_name  # AWS account name
-  cidr              = "10.5.0.0/16"           # VPC CIDR
-  cluster_version   = "1.28"                  # EKS cluster version
+  source           = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
+  name             = local.name             # EKS cluster name
+  region           = local.region           # AWS provider region
+  aws_account_name = local.aws_account_name # AWS account name
+  cidr             = "10.5.0.0/16"          # VPC CIDR
+  cluster_version  = "1.28"                 # EKS cluster version
 
   # EKS Managed Private Node Group
-  instance_types    = ["m6a.2xlarge"]         # EKS instance types
-  min_size          = 5                       # EKS node group min size
-  max_size          = 15                      # EKS node group max size
-  desired_size      = 8                       # EKS node group desired size
+  instance_types = ["m6a.2xlarge"] # EKS instance types
+  min_size       = 5               # EKS node group min size
+  max_size       = 15              # EKS node group max size
+  desired_size   = 8               # EKS node group desired size
 
   # Karpenter
-  create_karpenter_irsa = true                 # Create Karpenter IRSA
+  create_karpenter_irsa = true # Create Karpenter IRSA
 }
